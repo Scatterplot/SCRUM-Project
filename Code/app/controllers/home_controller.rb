b053a0cbd2a9bfe04b135de787311d9523750ce3
Cheryl H <cheryl.huber@my.csun.edu>
2015-04-28 13:51:32 -0700

    prettyfied user interaction

--- Code/app/controllers/home_controller.rb
+++ Code/app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
 	before_filter :authenticate_user! #, :only => [:index, :new]
   def index
-	end
-	def new
+  	@user = User.find(params[:id])
+  end
+  def new
 
-	end
-	def edit
+  end
+  def edit
 
-	end
+  end
 end
