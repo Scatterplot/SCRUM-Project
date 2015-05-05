145851c6fd68a73ed47cc3a05a01a88935077581
Scatterplot <Dog@DogBook-Pro.local>
2015-05-05 04:19:17 -0700

    Consolidation and security measures
    Consolidated authentication checks to application_controller.rb,
    implemented redirection to sign-up by default to avoid presenting
    unnecessary errors.

--- Code/app/controllers/application_controller.rb
+++ Code/app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :check_user_status, :except => [:index, :edit, :destroy, :new]
   protect_from_forgery with: :exception
+  
+  def check_user_status
+      if user_signed_in?
+          if current_user.role_id != 1
+              redirect_to ''
+          end
+      else
+        redirect_to '/users/sign_in'
+      end
+  end
 end
--- Code/app/controllers/home_controller.rb
+++ Code/app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
-	before_filter :authenticate_user! #, :only => [:index, :new]
   def index
-  	@user = User.find(params[:id])
+      
   end
   def new
 
